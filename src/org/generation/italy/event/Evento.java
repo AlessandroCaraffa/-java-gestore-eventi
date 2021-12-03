0f48f06291fb2227be909a32b7bc33d84fd50a9a
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-12-03 13:07:23 +0100

    aggiunte exceptions e iniziato main

--- src/org/generation/italy/event/Evento.java
+++ src/org/generation/italy/event/Evento.java
@@ -1,5 +1,95 @@
 package org.generation.italy.event;
+import java.time.LocalDate;
+
 
 public class Evento {
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati;
+	
+	
+	Evento(String titolo,LocalDate data,int postiTotali)throws Exception{
+		int dataPassata = data.compareTo(LocalDate.now());
+		if (dataPassata >= 0 ) {
+			if (postiTotali > 0) {
+				this.titolo = titolo;
+				this.data = data;
+				this.postiTotali = postiTotali;
+				this.postiPrenotati = 0;
+			}else {
+				throw new IllegalArgumentException("Numero posti errato");			
+			}
+		}else {
+			throw new IllegalArgumentException("Data errata");			
+		}
+		
+		
+	}
+	
+	public void prenota ()throws Exception {
+		int dataDisponibile = data.compareTo(LocalDate.now());
+		if (dataDisponibile >= 0 ) {
+			if (postiPrenotati < postiTotali) {
+				postiPrenotati++;
+			}else {
+				throw new IllegalArgumentException("Posti esauriti");
+			}
+		}else {
+			throw new IllegalArgumentException("Evento passato");
+		}
+		
+	}
+
+	public void disdici() {
+		int dataDisponibile = data.compareTo(LocalDate.now());
+		if (dataDisponibile >= 0) {
+			if (postiPrenotati < postiTotali) {
+				postiPrenotati--;
+			} else {
+				throw new IllegalArgumentException("Nessuna prenotazione");
+			}
+		} else {
+			throw new IllegalArgumentException("Evento passato");
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return data.toString() + titolo;
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	public String getTitolo() {
+		return titolo;
+	}
+
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+
+	public LocalDate getData() {
+		return data;
+	}
+
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
 
 }
--- src/org/generation/italy/event/Main.java
+++ src/org/generation/italy/event/Main.java
@@ -1,9 +1,47 @@
 package org.generation.italy.event;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Iseririre nome evento:");
+		String nomeEvento = scan.nextLine();
+		System.out.println("Inserire numero posti disponibili:");
+		int postiDisponibili = scan.nextInt();
+		System.out.println("Inserire giorno evento:");
+		int giornoEvento = scan.nextInt();
+		System.out.println("Inserire mese evento:");
+		int meseEvento = scan.nextInt();
+		System.out.println("Inserire anno evento:");
+		int annoEvento = scan.nextInt();
+		
+		LocalDate dataEvento = null; 
+		dataEvento = LocalDate.of(annoEvento, meseEvento, giornoEvento);
+		
+		
+		
+		
+		
+		
+		
+		
+		try {
+			Evento prova = new Evento(nomeEvento, dataEvento, postiDisponibili);
+			System.out.println(prova.toString());
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		System.out.println(dataEvento.compareTo(LocalDate.now()));
+//		String stampa = prova.toString();
+//		System.out.println(prova.getData(););
+		scan.close();
 
 	}
 
